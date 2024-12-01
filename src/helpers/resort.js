8e6f8facb8149f896e44f89bcb85d5564b3ace28
Victor K <dev@victorkarasev.ru>
2024-12-02 04:30:45 +0700

    sort function

---
+++ src/helpers/resort.js
@@ -0,0 +1,26 @@
+export const reSort = (input) => {
+  let temp = [...input];
+  let result = new Map;
+  let iterator = 0;
+
+  while (temp.length > 0) {
+    const randomIndex = Math.floor(Math.random() * temp.length);
+    const key = input[iterator];
+    let val = temp.splice(randomIndex, 1)[0];
+
+    if (key === val) {
+      // console.warn('warn pos', input[iterator]);
+      temp = [...input];
+      result = new Map;
+      iterator = 0;
+      val = 0;
+    }
+    
+    if (val) {
+      result.set(key, val)
+      iterator+=1;
+    }
+  }
+
+  return result;
+};
