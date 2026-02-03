515eaa15a32aa2ff30777863bcdc9122ddaf6a67
Victor K <dev@victorkarasev.ru>
2026-02-04 06:44:53 +0700

    links

--- src/containers/RecipientLine/RecipientLine.tsx
+++ src/containers/RecipientLine/RecipientLine.tsx
@@ -8,13 +8,16 @@ interface RecipientLineProps {
   lineType: LineType;
 }
 
-const TITLES:{
-  [value in LineType] : string;
-} = {
+const TITLES: Record<LineType, string> = {
   gender: 'Твой получатель',
   wishes: 'Пожелания',
   ozon_address: 'ozon',
   wb_address: 'wb',
+}
+
+const LINKS: Partial<Record<LineType, string>> = {
+  ozon_address: 'https://www.ozon.ru/',
+  wb_address: 'https://www.wildberries.ru/',
 }
 
 const RecipientLine = ({ value, lineType }: RecipientLineProps) => {
@@ -22,10 +25,23 @@ const RecipientLine = ({ value, lineType }: RecipientLineProps) => {
 
   return (
     <div>
-      <div className={`line-head ${defineSpecialStyles}`}>
-        {defineSpecialStyles ? TITLES[lineType].toLocaleUpperCase() : TITLES[lineType]}:
+      <div className="line-head">
+        {defineSpecialStyles ? (
+          <a
+            className={defineSpecialStyles}
+            href={LINKS[lineType]}
+            title={`Перейти на сайт ${TITLES[lineType]}`}
+            target='_blank'>
+              {TITLES[lineType].toLocaleUpperCase()}:
+          </a>
+        ) : (
+          <>
+            {TITLES[lineType]}:
+          </>
+        )}
       </div>
-
+      
+      
       {!defineSpecialStyles && (
         <div>{value}</div>
       )}
