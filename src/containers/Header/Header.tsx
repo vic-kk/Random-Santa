e4308b576523e3c62bc2c83ac468f7545f80b495
Victor K <dev@victorkarasev.ru>
2026-02-24 18:57:39 +0700

    change head

--- src/containers/Header/Header.tsx
+++ src/containers/Header/Header.tsx
@@ -9,27 +9,29 @@ interface HeaderProps {
 
 const Header = ({ number, adminUrl }: HeaderProps) => {
   return (
-    <div>
-      <p>
+    <div className='santa-header'>
+      <div>
         <img src={santaLogo} className="logo" width={250} alt="santa logo" />
-      </p>
-      <div className='title'>
-        Твой уникальный номер:
-      </div>
-      <div className="number">
-        <CopyToClipboard
-          value={number.toString()}
-          successMessage='Ваш номер скопирован'
-        >
-          {number}
-        </CopyToClipboard>
       </div>
       <div>
-        Твой номер уже сохранен на этой странице, но, <br/>
-        на всякий случай, <u><b>сфотографируй</b></u> или <u><b>запиши его</b></u>
-      </div>
-      <div>
-        <a className='tg' href={adminUrl} target='_blank'>группа в TG</a>
+        <div className='title'>
+          Твой уникальный номер:
+        </div>
+        <div className="number">
+          <CopyToClipboard
+            value={number.toString()}
+            successMessage='Ваш номер скопирован'
+          >
+            {number}
+          </CopyToClipboard>
+        </div>
+        <div>
+          Твой номер уже сохранен на этой странице, но, <br/>
+          на всякий случай, <u><b>сфотографируй</b></u> или <u><b>запиши его</b></u>
+        </div>
+        <div>
+          <a className='tg' href={adminUrl} target='_blank'>группа в TG</a>
+        </div>
       </div>
     </div>
   )
